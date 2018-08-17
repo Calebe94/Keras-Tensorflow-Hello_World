b923b57538192ab0380321212bce34515e4cd291
calebe94 <calebe.sep@hotmail.com>
2018-08-17 17:35:51 -0300

    <feat> added first test #wip

---
+++ first_train.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid('off')
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
